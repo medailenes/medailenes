f321fb2622eac10d763846eac47e1879ae5e9fae
medailenes <medailenes@gmail.com>
2024-06-09 18:04:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const countLikes2 = document.getElementById("countLikes2")
 function clickLike1(){
     let totalLikes1 = parseInt(countLikes1.value) + 1
     countLikes1.textContent = totalLikes1.toString()
-}
+  }
 function clickLike2(){
     let totalLikes2 = parseInt(countLikes2.value) + 1
     countLikes2.textContent = totalLikes2.toString()
@@ -28,10 +28,8 @@ btnDisLike2.addEventListener("click",DisLikes2)
 const comment = document.getElementById("comment")
 const submit = document.getElementById("submit")
 const commentbox = document.getElementById("commentbox")
-
 function submitComment() {
    commentbox.textContent += comment.value.toString() + "\n"
     comment.value=""
-
 }
 submit.addEventListener("click",submitComment)
